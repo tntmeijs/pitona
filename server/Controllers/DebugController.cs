4897ed1655c0dfa1e57af9c5cb35836e845ac5b7
Tahar Meijs <tntmeijs@gmail.com>
2022-08-16 22:19:45 +0200

    feat: add simple .NET server

---
+++ server/Controllers/DebugController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using server.Models.System;
+
+namespace server.Controllers;
+
+[ApiController]
+[Route("system")]
+public class SystemController : ControllerBase
+{
+    [HttpGet("status")]
+    public ActionResult<StatusModel> GetStatus()
+    {
+        return new StatusModel { CpuTemperature = 69420.0d };
+    }
+}
